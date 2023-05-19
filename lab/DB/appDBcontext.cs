ad25c07537e4412362564d70b78a94ec11482b41
bryzgalov <bryzgalov@greendatasoft.ru>
2023-05-19 11:24:23 +0500

    Initial commit

---
+++ lab/DB/appDBcontext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using lab.Models;
+
+namespace lab.DB
+{
+
+    public class appDBcontext: DbContext
+    {
+        public appDBcontext(DbContextOptions<appDBcontext> cock):base(cock)
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ToDoItem>().ToTable("ToDoTable");
+        }
+        public DbSet<ToDoItem> ToDoTable { get; set; }
+    }
+}
---
+++ lab/Models/ToDoItem.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace lab.Models
+{
+    public class ToDoItem
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
